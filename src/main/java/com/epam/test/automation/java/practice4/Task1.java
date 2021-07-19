f65ca4d669f7d0541083229f8c801deceb4572f7
Mykola <legisus@gmail.com>
2021-07-19 15:44:01 +0200

    5

--- src/main/java/com/epam/test/automation/java/practice4/Task1.java
+++ src/main/java/com/epam/test/automation/java/practice4/Task1.java
@@ -19,43 +19,17 @@ public class Task1 {
 
         if (order == SortOrder.ASC){
             boolean isAscending = false;
-            if (IntStream.range(0, array.length - 1).noneMatch(i -> array[i] > array[i + 1])){
+            if (IntStream.range(0, array.length - 1).noneMatch(i -> array[i] > array[i + 1])) {
                 isAscending = true;
-            }else if (IntStream.range(0, array.length - 1).noneMatch(i -> array[i] < array[i + 1])){
-                isAscending = false;
             }
             return isAscending;
         }if (order == SortOrder.DESC){
             boolean isDesc = false;
-            if (IntStream.range(0, array.length - 1).noneMatch(i -> array[i] < array[i + 1])){
+            if (IntStream.range(0, array.length - 1).noneMatch(i -> array[i] < array[i + 1])) {
                 isDesc = true;
-            }else if (IntStream.range(0, array.length - 1).noneMatch(i -> array[i] > array[i + 1])){
-                isDesc = false;
             }
             return isDesc;
         }
         return false;
     }
-
-/*
-TeacherSuite > TeacherTests > com.epam.test.automation.java.practice4.TestsTeacherTask1 > testArrayIsSortedWithNullArgs FAILED
-
-    org.testng.TestException:
-
-    Expected exception of type class java.lang.IllegalArgumentException but got java.lang.NullPointerException
-
-
-
-        Caused by:
-
-        java.lang.NullPointerException
- */
-
-    /*
-    TeacherSuite > TeacherTests > com.epam.test.automation.java.practice4.TestsTeacherTask1 > arrayIsSortedWithIllegalArgumentException[0]([I@75b9adfd) FAILED
-
-    org.testng.TestException:
-
-    Method TestsTeacherTask1.arrayIsSortedWithIllegalArgumentException([I)[pri:0, instance:com.epam.test.automation.java.practice4.TestsTeacherTask1@3d554406] should have thrown an exception of type class java.lang.IllegalArgumentException
-     */
 }
